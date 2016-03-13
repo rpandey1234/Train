85cb5fc1d365dae044941f0f21176a7027bb2eae
Rahul Pandey <rahul@pinterest.com>
2016-03-12 23:35:55 -0800

    viewholder patter for user autocomplete

--- app/src/main/java/com/franklinho/vidtrain_android/adapters/UsersAdapter.java
+++ app/src/main/java/com/franklinho/vidtrain_android/adapters/UsersAdapter.java
@@ -14,16 +14,22 @@ import com.franklinho.vidtrain_android.models.User;
 import com.parse.ParseUser;
 
 import java.util.List;
+import java.util.zip.Inflater;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by rahul on 3/11/16.
  */
 public class UsersAdapter extends ArrayAdapter<ParseUser> {
     Context mContext;
+    private final LayoutInflater inflater;
 
     public UsersAdapter(Context context, List<ParseUser> users) {
         super(context, R.layout.item_user, users);
         mContext = context;
+        inflater = LayoutInflater.from(context);
     }
 
     @Override
@@ -31,23 +37,29 @@ public class UsersAdapter extends ArrayAdapter<ParseUser> {
         // Get the data item for this position
         ParseUser user = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
+        ViewHolder viewHolder;
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_user, parent, false);
+            convertView = inflater.inflate(R.layout.item_user, parent, false);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
-        // Lookup view for data population
-        TextView tvName = (TextView) convertView.findViewById(R.id.tvName);
-        ImageView ivProfileImage = (ImageView) convertView.findViewById(R.id.ivProfileImage);
-
         // Populate the data into the template view using the data object
-        tvName.setText(User.getName(user));
+        viewHolder.tvName.setText(User.getName(user));
         String profileImageUrl = User.getProfileImageUrl(user);
-        Glide.with(mContext).load(profileImageUrl).into(ivProfileImage);
+        Glide.with(mContext).load(profileImageUrl).into(viewHolder.ivProfileImage);
         // Return the completed view to render on screen
         return convertView;
     }
 
     // View lookup cache
-    private static class ViewHolder {
-        TextView name;
+    static class ViewHolder {
+        @Bind(R.id.tvName) TextView tvName;
+        @Bind(R.id.ivProfileImage) ImageView ivProfileImage;
+
+        public ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
     }
 }
