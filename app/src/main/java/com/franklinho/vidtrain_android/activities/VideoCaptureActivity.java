d94b54ee0579f0596b2f1d481a8c45a1e5ee7225
vimalathithan <vimalathithanit@gmail.com>
2016-03-13 21:23:16 -0700

    Added Camera frameoverlay

---
+++ app/src/main/java/com/franklinho/vidtrain_android/activities/VideoCaptureActivity.java
@@ -0,0 +1,20 @@
+package com.franklinho.vidtrain_android.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by VRAJA03 on 3/13/2016.
+ */
+public class VideoCaptureActivity extends AppCompatActivity {
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return null;
+    }
+
+}
