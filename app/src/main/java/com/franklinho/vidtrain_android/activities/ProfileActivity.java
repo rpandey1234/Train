ff4bc177fca745b56d05739797e3d7b2ea4610e0
vimalathithan <vimalathithanit@gmail.com>
2016-03-13 19:52:57 -0700

    Added back button on profile activity

--- app/src/main/java/com/franklinho/vidtrain_android/activities/ProfileActivity.java
+++ app/src/main/java/com/franklinho/vidtrain_android/activities/ProfileActivity.java
@@ -7,24 +7,37 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.franklinho.vidtrain_android.R;
 import com.franklinho.vidtrain_android.fragments.PopularFragment;
 import com.franklinho.vidtrain_android.fragments.UserInfoFragment;
 import com.parse.ParseUser;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class ProfileActivity extends AppCompatActivity {
 
     public static final String USER_ID = "userId";
     PopularFragment userProfileFragment;
+    @Bind(R.id.toolbar) Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
 
         String userId = getIntent().getStringExtra(USER_ID);
         if (userId == null) {
