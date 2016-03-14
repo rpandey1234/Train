22cfea619270b916957ed2530169fea26ec73616
Rahul Pandey <rahul@pinterest.com>
2016-03-13 17:55:33 -0700

    fixes to creation flow, finish refactor

--- app/src/main/java/com/franklinho/vidtrain_android/utilities/VideoPlayer.java
+++ app/src/main/java/com/franklinho/vidtrain_android/utilities/VideoPlayer.java
@@ -1,7 +1,7 @@
 package com.franklinho.vidtrain_android.utilities;
 
 import com.franklinho.vidtrain_android.models.DynamicVideoPlayerView;
-import com.franklinho.vidtrain_android.networking.VidtrainApplication;
+import com.volokh.danylo.video_player_manager.PlayerMessageState;
 import com.volokh.danylo.video_player_manager.manager.PlayerItemChangeListener;
 import com.volokh.danylo.video_player_manager.manager.SingleVideoPlayerManager;
 import com.volokh.danylo.video_player_manager.meta.MetaData;
@@ -15,9 +15,7 @@ public class VideoPlayer {
     public VideoPlayer() {
         videoPlayerManager = new SingleVideoPlayerManager(new PlayerItemChangeListener() {
             @Override
-            public void onPlayerItemChanged(MetaData metaData) {
-
-            }
+            public void onPlayerItemChanged(MetaData metaData) {}
         });
     }
 
@@ -26,7 +24,14 @@ public class VideoPlayer {
     }
 
     public static void playVideo(DynamicVideoPlayerView videoPlayer, String videoUrl) {
-        final SingleVideoPlayerManager player = VideoPlayer.getVideoPlayer();
-        player.playNewVideo(null, videoPlayer, videoUrl);
+        videoPlayerManager.playNewVideo(null, videoPlayer, videoUrl);
+    }
+
+    public static PlayerMessageState getState() {
+        return videoPlayerManager.getCurrentPlayerState();
+    }
+
+    public static void stop() {
+        videoPlayerManager.stopAnyPlayback();
     }
 }
