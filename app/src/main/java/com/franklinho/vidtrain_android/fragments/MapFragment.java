dcd0a6eac91eee2ee1f65d0a83d6d139bc5b0069
franklineho@gmail.com <franklinho@Franklins-MacBook-Pro.local>
2016-03-15 19:09:02 -0700

    push up latest changes

--- app/src/main/java/com/franklinho/vidtrain_android/fragments/MapFragment.java
+++ app/src/main/java/com/franklinho/vidtrain_android/fragments/MapFragment.java
@@ -170,7 +170,7 @@ public class MapFragment extends Fragment implements
         query.setSkip(currentSize);
         query.setLimit(5);
         final BitmapDescriptor defaultMarker = BitmapDescriptorFactory.defaultMarker(
-                BitmapDescriptorFactory.HUE_CYAN);
+                BitmapDescriptorFactory.HUE_AZURE);
         query.findInBackground(new FindCallback<VidTrain>() {
             @Override
             public void done(List<VidTrain> objects, ParseException e) {
