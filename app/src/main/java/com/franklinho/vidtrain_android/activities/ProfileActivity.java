2730182538376db94c40ac4eb876a1eb9e60a683
Rahul Pandey <rahul@pinterest.com>
2016-03-27 22:48:49 -0700

    Add logout ability

--- app/src/main/java/com/franklinho/vidtrain_android/activities/ProfileActivity.java
+++ app/src/main/java/com/franklinho/vidtrain_android/activities/ProfileActivity.java
@@ -7,12 +7,16 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.franklinho.vidtrain_android.R;
 import com.franklinho.vidtrain_android.fragments.PopularFragment;
 import com.franklinho.vidtrain_android.fragments.UserCreationsFragment;
 import com.franklinho.vidtrain_android.fragments.UserInfoFragment;
+import com.parse.LogOutCallback;
+import com.parse.ParseException;
 import com.parse.ParseUser;
 
 import butterknife.Bind;
@@ -59,5 +63,28 @@ public class ProfileActivity extends AppCompatActivity {
 
             ft.commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.actionLogout) {
+            ParseUser.logOutInBackground(new LogOutCallback() {
+                @Override
+                public void done(ParseException e) {
+                    Intent intent = new Intent(getApplicationContext(), LogInActivity.class);
+                    startActivity(intent);
+                }
+            });
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
