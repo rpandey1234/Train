bf98b956f6275540df773da74af2dab7ee52738e
Vimalathithan Rajasekaran <vimalathithanit@gmail.com>
2016-08-16 09:23:22 -0700

    Code formatting

--- app/src/main/java/com/trainapp/utilities/Utility.java
+++ app/src/main/java/com/trainapp/utilities/Utility.java
@@ -219,7 +219,6 @@ public class Utility {
         if (launcherClassName == null) {
             return;
         }
-
         Intent intent = new Intent("android.intent.action.BADGE_COUNT_UPDATE");
         intent.putExtra("badge_count", count);
         intent.putExtra("badge_count_package_name", context.getPackageName());
@@ -228,12 +227,9 @@ public class Utility {
     }
 
     public static String getLauncherClassName(Context context) {
-
         PackageManager pm = context.getPackageManager();
-
         Intent intent = new Intent(Intent.ACTION_MAIN);
         intent.addCategory(Intent.CATEGORY_LAUNCHER);
-
         List<ResolveInfo> resolveInfos = pm.queryIntentActivities(intent, 0);
         for (ResolveInfo resolveInfo : resolveInfos) {
             String pkgName = resolveInfo.activityInfo.applicationInfo.packageName;
